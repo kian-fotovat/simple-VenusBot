cd3e907d83642e55f1afd12e46a83d010be47ca6
kian-fotovat <kianf2011@gmail.com>
2025-10-04 12:29:40 -0500

    I like the emojis more actually.

--- embed_views/music_buttons.py
+++ embed_views/music_buttons.py
@@ -7,29 +7,29 @@ class MusicButtons(discord.ui.View):
         self.tree = client.tree
         self.musicController = musicController
 
-    @discord.ui.button(label="▶", style=discord.ButtonStyle.secondary, row=0)
+    @discord.ui.button(label="▶️", style=discord.ButtonStyle.secondary, row=0)
     async def PauseResume_Button(self, interaction: discord.Interaction, Button: discord.ui.Button):
         response = await self.musicController.pauseSong()
         if response:
-            Button.label = "▶"
+            Button.label = "▶️"
             await interaction.response.edit_message(view=self)
         else:
-            Button.label = "⏸"
+            Button.label = "⏸️"
             await interaction.response.edit_message(view=self)
 
-    @discord.ui.button(label="⏭", style=discord.ButtonStyle.secondary, row=0)
+    @discord.ui.button(label="⏭️", style=discord.ButtonStyle.secondary, row=0)
     async def Skip_Button(self, interaction: discord.Interaction, Button: discord.ui.Button):
         await self.tree.get_command("skip").callback(interaction)
 
-    @discord.ui.button(label="⏹", style=discord.ButtonStyle.secondary, row=0)
+    @discord.ui.button(label="⏹️", style=discord.ButtonStyle.secondary, row=0)
     async def Stop_Button(self, interaction: discord.Interaction, Button: discord.ui.Button):
         await self.tree.get_command("stop").callback(interaction)
 
-    @discord.ui.button(label="⟳", style=discord.ButtonStyle.secondary, row=0)
+    @discord.ui.button(label="🔁", style=discord.ButtonStyle.secondary, row=0)
     async def Loop_Button(self, interaction: discord.Interaction, Button: discord.ui.Button):
         await self.tree.get_command("loop").callback(interaction)
 
-    @discord.ui.button(label="⇄", style=discord.ButtonStyle.secondary, row=1)
+    @discord.ui.button(label="🔀", style=discord.ButtonStyle.secondary, row=1)
     async def Shuffle_Button(self, interaction: discord.Interaction, Button: discord.ui.Button):
         await self.tree.get_command("shuffle").callback(interaction)
 
@@ -37,6 +37,6 @@ class MusicButtons(discord.ui.View):
     async def Queue_Button(self, interaction: discord.Interaction, Button: discord.ui.Button):
         await self.tree.get_command("queue").callback(interaction)
 
-    @discord.ui.button(label="⊘", style=discord.ButtonStyle.secondary, row=1)
+    @discord.ui.button(label="❌", style=discord.ButtonStyle.secondary, row=1)
     async def Kick_Button(self, interaction: discord.Interaction, Button: discord.ui.Button):
         await self.tree.get_command("dc").callback(interaction)
